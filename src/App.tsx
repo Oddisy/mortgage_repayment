c9c5888003c87f14857139450a176bd94c5cef8a
Odunayo <odunayoogunleye200@gmail.com>
2025-03-21 01:20:49 +0100

    created file

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css"
+
+function App() {
+
+  return (
+    
+    <div className="container">
+      <div className="container_sub-container">
+        <div className="content-container">
+      <div className="left-container">dfbga</div>
+      <div className="right-container">fsb</div>
+      </div>
+      </div>
+    </div>
+
+  
+  )
+}
+
+export default App
