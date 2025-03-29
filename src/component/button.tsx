cf6697b8aba4505a3113139b6669c45ed8588d5d
Odunayo <odunayoogunleye200@gmail.com>
2025-03-29 21:53:53 +0100

    fix: correct interface for button type

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -1,14 +1,8 @@
 import "../css/button.css"
 import {ButtonProps} from "../../interface"
-export const Button = ({
-	btnText,
-	btnClassName,
-	btnIcon,
-	btnType,
-	onClick,
-}: ButtonProps) => {
+export const Button = ({ btnText, btnClassName, btnIcon, btnType }: ButtonProps) => {
 	return (
-		<button type={btnType} onClick={onClick} className={btnClassName}>
+		<button type={btnType} className={btnClassName}>
 			<div>
 				<div>{btnIcon}</div>
 				<p>{btnText}</p>
